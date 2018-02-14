ea3de3a9f999d2b224d3e02a9c9d88bc5afea14a
naprasad <Nagendra.Prasad@misys.com>
2018-02-14 14:46:14 +0530

    vars\call_perl.groovy

--- vars/call_perl.groovy
+++ vars/call_perl.groovy
@@ -3,7 +3,6 @@
 def call(){
 		bat 'DATE /T'
 		bat 'TIME /T'
-
 		bat 'perl \\\\blrwinopfilsrv01\\OpicsCMRE_Work\\CMRE\\NP_Stuff\\scripts\\test.pl'
-
+		bat '\\\\blrwinopfilsrv01\\OpicsCMRE_Work\\CMRE\\NP_Stuff\\scripts\\Test_1.bat'
 }
