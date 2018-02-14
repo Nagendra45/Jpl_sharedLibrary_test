332cb5cb0bae524d818d701b41b87637792cefdc
naprasad <Nagendra.Prasad@misys.com>
2018-02-14 14:55:25 +0530

    File has been committed

--- vars/call_perl.groovy
+++ vars/call_perl.groovy
@@ -4,5 +4,5 @@ def call(){
 		bat 'DATE /T'
 		bat 'TIME /T'
 		bat 'perl \\\\blrwinopfilsrv01\\OpicsCMRE_Work\\CMRE\\NP_Stuff\\scripts\\test.pl'
-		//bat '\\\\blrwinopfilsrv01\\OpicsCMRE_Work\\CMRE\\NP_Stuff\\scripts\\Test_1.bat'
+		bat '\\\\blrwinopfilsrv01\\OpicsCMRE_Work\\CMRE\\NP_Stuff\\scripts\\Test_1.bat'
 }
